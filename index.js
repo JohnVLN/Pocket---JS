621f2461f2cb7da5b6955f1720a6dbb4e683751e
JoonathanVL <johnvllanos@gmail.com>
2024-09-15 16:00:58 -0300

    First commit.

---
+++ index.js
@@ -0,0 +1,18 @@
+//a unique goal
+let goal = {
+    value: 'read a book a month',
+    checked: false,
+}
+
+//the list of goals
+let goals = [
+    goal, {
+        value:'run once a week',
+        chacked: false,
+    }
+]
+//reading se second item of the list
+console.log(goals[1].value)
+//reading the entire list
+console.log(goals)
+
