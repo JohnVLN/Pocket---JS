8e209ace2696b15e35c6a49f50d2cc64b4187446
JoonathanVL <johnvllanos@gmail.com>
2024-09-15 22:40:15 -0300

    Created basic list functionality.

--- lesson02.js
+++ lesson02.js
@@ -1,9 +1,32 @@
-// Importing 'select' from 'inquirer' using the function 'require()'
-const { select } = require('@inquirer/prompts')
+// Importing 'select' and 'input' from 'inquirer' using the function 'require()'
+const { select, input } = require('@inquirer/prompts')
+
+// Example of item
+let item = {
+    value: 'Run once a week',
+    checked: false,
+}
+// List of goals (items)
+let goals = [ item ]
+
+const addGoal = async () => {
+    const goal = await input({ message: "Write down a new goal... \n> " })
+
+    // In case the user does not input anything
+    if(goal.length == 0) {
+        console.log('The slot cannot be empty')
+        return
+    }
+
+    // Push user input into the goals list
+    goals.push({
+        value: goal, checked: false
+    })
+}
 
 const start = async () => {
     while(true) {
-
+        // Options given to the user in the menu
         const option = await select({
             message: 'Menu > ',
             choices: [
@@ -20,7 +43,8 @@ const start = async () => {
 
         switch(option){ 
             case 'add':
-                console.log("Let's add a goal to your list.")
+                await addGoal()
+                console.log(goals)
                 break
             case 'quit':
                 console.log("Till next time!")
