ca0d6b0e147e078f4e7892ce252509f960748773
JoonathanVL <johnvllanos@gmail.com>
2024-09-15 21:32:45 -0300

    Formulates the menu with 'inquire' module.

--- lesson02.js
+++ lesson02.js
@@ -1,14 +1,31 @@
-/* 2 ways of creating a function:
-function start() {
-    console.log('the function works!')
-} 
-and */
-const start = () => {
-    let count = 0
-    // creating a simple loop
-    while(count <= 10){
-        console.log(count)
-        count += 1
+// Importing 'select' from 'inquirer' using the function 'require()'
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+    while(true) {
+
+        const option = await select({
+            message: 'Menu > ',
+            choices: [
+                {
+                    name: "Add an item.",
+                    value: "add"
+                },
+                {
+                    name: "Quit",
+                    value: "quit"
+                }
+            ]
+        })
+
+        switch(option){ 
+            case 'add':
+                console.log("Let's add a goal to your list.")
+                break
+            case 'quit':
+                console.log("Till next time!")
+                return
+        }
     }
 }
 start()
