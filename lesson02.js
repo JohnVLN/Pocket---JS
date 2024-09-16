eb4cc793ebb976ccaa3803fe983af36cc74eb6c7
JoonathanVL <johnvllanos@gmail.com>
2024-09-16 17:46:50 -0300

    Added listing and ticking functionality.

--- lesson02.js
+++ lesson02.js
@@ -1,5 +1,5 @@
 // Importing 'select' and 'input' from 'inquirer' using the function 'require()'
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 // Example of item
 let item = {
@@ -24,6 +24,35 @@ const addGoal = async () => {
     })
 }
 
+const listGoals = async () => {
+    // Selection menu that copies items from the goals list to display to user
+    const responses = await checkbox({
+        message: "> Use the arrows to select\n> Use space to check an item off\n> Hit enter when you are done.\n",
+        choices: [...goals],
+        instructions: false,
+    })
+
+    if(responses.lenght == 0) {
+        console.log('No items have been selected!')
+        return
+    }
+
+    // Sets all items as unchecked inside selection menu, allowing to mark goals as 'not done'
+    responses.forEach((g) => {
+        g.checked = false
+    })
+
+    // using .forEach on the array of selected items of the list
+    responses.forEach((response) => {
+        // using .find to get the item that matches the same value of the selected item
+        const goal = goals.find((g) => {
+            return g.value == response
+        })
+        goal.checked = true
+    })
+    console.log('Goal(s) have been ticked off as Achieved!')
+}
+
 const start = async () => {
     while(true) {
         // Options given to the user in the menu
@@ -31,8 +60,12 @@ const start = async () => {
             message: 'Menu > ',
             choices: [
                 {
-                    name: "Add an item.",
+                    name: "Add an item",
                     value: "add"
+                },
+                {
+                    name: "List all goals",
+                    value: "list"
                 },
                 {
                     name: "Quit",
@@ -45,6 +78,9 @@ const start = async () => {
             case 'add':
                 await addGoal()
                 console.log(goals)
+                break
+            case 'list':
+                await listGoals()
                 break
             case 'quit':
                 console.log("Till next time!")
